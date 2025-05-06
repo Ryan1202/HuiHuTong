255b160929454c436e4a8d38dde58754ded20a8d
Ryan1202 <ryan1202@foxmail.com>
2025-05-06 21:58:57 +0800

    feat: add GithubRelease log

--- app/src/main/java/com/ryan1202/huihutong/GithubRelease.kt
+++ app/src/main/java/com/ryan1202/huihutong/GithubRelease.kt
@@ -1,5 +1,6 @@
 package com.ryan1202.huihutong
 
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -20,22 +21,29 @@ suspend fun fetchLatestRelease():GithubRelease? =
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(url).build()
-
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) return@withContext null
-            val jsonString = response.body()?.string() ?: return@withContext null
-            return@withContext try {
-                val json = JSONObject(jsonString)
-                val tagName = json.getString("tag_name")
-                val name = json.optString("name")
-                val body = json.optString("body")
-                val htmlUrl = json.getString("html_url")
-                val publishedAt = json.optString("published_at")
-                GithubRelease(tagName, name, body, publishedAt, htmlUrl)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                null
+        try{
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    Log.e("GitHubRelease", "Failed response: ${response.code()}")
+                    return@withContext null
+                }
+                val jsonString = response.body()?.string() ?: return@withContext null
+                return@withContext try {
+                    val json = JSONObject(jsonString)
+                    val tagName = json.getString("tag_name")
+                    val name = json.optString("name")
+                    val body = json.optString("body")
+                    val htmlUrl = json.getString("html_url")
+                    val publishedAt = json.optString("published_at")
+                    GithubRelease(tagName, name, body, publishedAt, htmlUrl)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    null
+                }
             }
+        } catch (e: Exception) {
+            Log.e("GitHubRelease", "Request failed", e)
+            null
         }
     }
 
